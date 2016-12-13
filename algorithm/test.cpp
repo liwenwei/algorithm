98f15657b38621c63c2993e9e670dd2e04cfb7fa
liwenwei <liwenwei06@126.com>
2016-12-13 19:57:17 +0800

    Test “Longest Substring Without Repeating Characters”

--- algorithm/test.cpp
+++ algorithm/test.cpp
@@ -1,12 +1,93 @@
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-bool isAnagram(string s, string t){
+int lengthOfLongestSubstring(string s)
+{
+    vector<int> charIndex(256, -1);
+    int longest = 0, m = 0;
 
+    for (int i = 0; i < s.length(); i++)
+    {
+        m = max(charIndex[s[i]] + 1, m);
+        charIndex[s[i]] = i;
+        longest = max(longest, i - m + 1);
+    }
+
+    return longest;
 }
 
+bool isAnagram(string s, string t){
+    if (s.length() != t.length()) return false;
+
+    unordered_map<char, int> counts;
+    for (size_t i = 0; i < s.length(); i++)
+    {
+        counts[s[i]]++;
+        counts[t[i]]--;
+    }
+    for (auto count : counts)
+    {
+        if (count.second) return false;
+    }
+
+    return true;
+}
+
+vector<int> twoSum(vector<int>& numbers, int target)
+{
+    vector<int> indexVec;
+
+    size_t len = numbers.size();
+    if (len < 2) return indexVec;
+
+    for (size_t i = 1; i < len; i++)
+    {
+        for (size_t j = i + 1; j < len; j++)
+        {
+            if ((numbers[i] + numbers[j]) == target)
+            {
+                indexVec.push_back(i + 1);
+                indexVec.push_back(j + 1);
+                return indexVec;
+            }
+        }
+    }
+
+    return indexVec;
+}
+
+vector<int> twoSumBetter(vector<int>& numbers, int target)
+{
+    vector<int> indexVec;
+    size_t len = numbers.size();
+    if (len < 2) return indexVec;
+
+    int left = 0, right = len - 1;
+    while (left < right)
+    {
+        int value = numbers[left] + numbers[right];
+        if (value == target)
+        {
+            indexVec.push_back(left + 1);
+            indexVec.push_back(right + 1);
+            break;
+        }
+        else if (value > target)
+        {
+            right--;
+        }
+        else
+        {
+            left++;
+        }
+    }
+    return indexVec;
+}
 
 int removeDuplicates(vector<int> &nums)
 {
@@ -33,6 +114,6 @@ int removeDuplicates(vector<int> &nums)
 
 int main()
 {
-    vector<int> nums = { 1, 2, 2 };
-    removeDuplicates(nums);
+    string str = "abcabcbb";
+    cout << lengthOfLongestSubstring(str) << endl;
 }
