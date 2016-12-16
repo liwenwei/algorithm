c6f26ad5fbb3e2757be15318436ff1b5b42b487a
liwenwei <liwenwei06@126.com>
2016-12-16 14:45:16 +0800

    Optimize "Implement strStr()"

--- algorithm/str.cpp
+++ algorithm/str.cpp
@@ -5,30 +5,15 @@ using namespace std;
 
 int strStr(string haystack, string needle)
 {
-    if (needle.length() > haystack.length()) return -1;
-    if (haystack == needle) return 0;
+    if (haystack.length() < needle.length()) return -1;
+    if (haystack.length() == needle.length() || needle == "") return 0;
 
-    bool isEqual = false;
-    for (size_t i = 0; i < haystack.length(); i++)
+    for (size_t i = 0; i < haystack.length() - needle.length() + 1; i++)
     {
-        int begin = i;
         for (size_t j = 0; j < needle.length(); j++)
         {
-            if (needle[j] == haystack[begin])
-            {
-                isEqual = true;
-            }
-            else
-            {
-                isEqual = false;
-                break;
-            }
-            begin++;
-        }
-
-        if (isEqual)
-        {
-            return i;
+            if (needle[j] != haystack[i + j]) break;
+            if (j == needle.length() - 1) return i;
         }
     }
 
@@ -37,7 +22,7 @@ int strStr(string haystack, string needle)
 
 int main()
 {
-    string s1 = "";
+    string s1 = "a";
     string s2 = "";
     cout << strStr(s1, s2) << endl;
 }
