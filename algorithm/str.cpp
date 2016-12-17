79cf60be61e433aa1b8b3c411486cb0aa290319e
liwenwei <liwenwei06@126.com>
2016-12-17 23:26:13 +0800

    Test "String to Integer (atoi)"

--- algorithm/str.cpp
+++ algorithm/str.cpp
@@ -20,6 +20,36 @@ int strStr(string haystack, string needle)
     return -1;
 }
 
+int atoi(string str) {
+    int index = 0, sign = 1, total = 0;
+    //1. Empty string
+    if (str.length() == 0) return 0;
+
+    //2. Remove Spaces
+    while (str[index] == ' ' && index < str.length())
+        index++;
+
+    //3. Handle signs
+    if (str[index] == '+' || str[index] == '-'){
+        sign = str[index] == '+' ? 1 : -1;
+        index++;
+    }
+
+    //4. Convert number and avoid overflow
+    while (index < str.length()){
+        int digit = str[index] - '0';
+        if (digit < 0 || digit > 9) break;
+
+        //check if total will be overflow after 10 times and add digit
+        if (INT_MAX / 10 < total || INT_MAX / 10 == total && INT_MAX % 10 < digit)
+            return sign == 1 ? INT_MAX : INT_MIN;
+
+        total = 10 * total + digit;
+        index++;
+    }
+    return total * sign;
+}
+
 int main()
 {
     string s1 = "a";
