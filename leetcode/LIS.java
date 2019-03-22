f52903a995c01843ed54ed5f818a5f98df9482a8
FAREAST\v-wenwli <v-wenwli@microsoft.com>
2019-03-22 19:36:30 +0800

    Leetcode： 1. get the closest pairs by given a sorted array and sum 2. Longest Increasing Subsequence

---
+++ leetcode/LIS.java
@@ -0,0 +1,20 @@
+/*****************************************************************
+ * 300.Longest Increasing Subsequence(最长递增子序列)
+ * 
+ * Given an unsorted array of integers, find the length of longest increasing subsequence.
+ * 
+ * @author liwenwei
+ * 
+ ****************************************************************/
+
+public class LIS {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public int lengthOfLIS(int[] nums) {
+        
+    }
+	
+}
---
+++ leetcode/SumClosest.java
@@ -0,0 +1,31 @@
+/*****************************************************************
+ * 已知一个升序的数组array，再给定一个定值sum，现在要找出数组array中哪两个元素
+ * 的和最接近于这个定值sum，并输出这两个元素
+ * 
+ * @author liwenwei
+ * 
+ ****************************************************************/
+
+public class SumClosest {
+
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5,6,7,8,9};
+		sumClosest(a, 15);
+	}
+
+	public static void sumClosest(int[] a, int sum) {
+		int lt = 0;
+		int rt = a.length - 1;
+		while (lt < rt) {
+			int val = a[lt] + a[rt];
+			if (val == sum) {
+				System.out.printf("%d - %d\n", a[lt], a[rt]);
+				return;
+			} else if (val > sum) {
+				rt--;
+			} else if (val < sum) {
+				lt++;
+			}
+		}
+	}
+}
